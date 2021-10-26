e42acb86570dc0d5bae394efa71172e5553cd6b3
p22mbonilla <p22mbonilla@fairfieldprep.org>
2021-10-26 10:20:43 -0400

    add file

---
+++ hw5-2.py
@@ -0,0 +1,24 @@
+# Author MB 10/26/2021
+
+import math
+
+# Question 1
+a = math.isqrt(549)
+print(a)
+
+# Question 2
+b = math.sin(0)
+print(b)
+
+# Question 3
+c = math.cos(math.pi * 2)
+print(c)
+
+# Question 4
+d = math.
+# Question 5
+# Question 6
+# Question 7
+# Question 8
+# Question 9
+# Question 10
