0b1be95256f869f07a12c02e9656e8f0eacb8e20
p22mbonilla <p22mbonilla@fairfieldprep.org>
2021-10-29 11:10:31 -0400

    complete

--- hw5-2.py
+++ hw5-2.py
@@ -15,10 +15,28 @@ c = math.cos(math.pi * 2)
 print(c)
 
 # Question 4
-d = math.
+d = math.pow(2.5, 5)
+
 # Question 5
+ea = math.e
+print(ea)
+
 # Question 6
+f = math.e ** 4
+print(f)
+
 # Question 7
+g = math.ceil(-12.9)
+print(g)
+
 # Question 8
+j = math.floor(7.25)
+print(j)
+
 # Question 9
+k = math.log10(1000) * 2
+print(k)
+
 # Question 10
+m = math.radians(180)
+print(m)
